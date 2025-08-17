94be660fba2df1ebafde55e511c034dab225fa8e
TheDudeInTheRedSuit <kylelsturrock@gmail.com>
2025-08-17 18:32:34 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,22 @@
 import random
+
+board = ['-','-','-','-','-','-','-','-','-',]
+
+def printboard():
+    print(board[0], end='')
+    print('|', end='')
+    print(board[1], end='')
+    print('|', end='')
+    print(board[2])
+    print(board[3], end='')
+    print('|', end='')
+    print(board[4], end='')
+    print('|', end='')
+    print(board[5])
+    print(board[6], end='')
+    print('|', end='')
+    print(board[7], end='')
+    print('|', end='')
+    print(board[8])
+
+printboard()
